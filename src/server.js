d49ddf7b6bbe42edbe186f1f02eda9f65b98ca9c
Peter Sonny Kwamina Fraikue <sonnyfraikue@gmail.com>
2022-04-11 13:27:12 +0100

    taboola pixel

--- src/server.js
+++ src/server.js
@@ -48,6 +48,20 @@ export const renderApp = (req, res) => {
       ${helmet.title.toString()}
         ${helmet.meta.toString()}
       ${cssLinksFromAssets(assets, "client")}
+      <!-- Taboola Pixel Code -->
+<script type='text/javascript'>
+  window._tfa = window._tfa || [];
+  window._tfa.push({notify: 'event', name: 'page_view', id: 1451779});
+  !function (t, f, a, x) {
+         if (!document.getElementById(x)) {
+            t.async = 1;t.src = a;t.id=x;f.parentNode.insertBefore(t, f);
+         }
+  }(document.createElement('script'),
+  document.getElementsByTagName('script')[0],
+  '//cdn.taboola.com/libtrc/unip/1451779/tfa.js',
+  'tb_tfa_script');
+</script>
+<!-- End of Taboola Pixel Code -->
   </head>
   <body>
       <div id="root">${markup}</div>
